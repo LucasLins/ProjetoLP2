f37b5fe294932585413329d4ae6d8cc1d09307b0
Thalyson Rocha <thalysonufpb@gmail.com>
2021-05-08 12:11:47 -0300

    Criação da Classe Gestor

---
+++ src/model/Gestor.java
@@ -0,0 +1,77 @@
+package model;
+
+public class Gestor {
+
+    public String nome;
+    public String sexo;
+    public String cpf;
+    public String telefone;
+    public String endereco;
+    
+    
+    public Gestor(String nome, String sexo, String cpf, String telefone, String endereco) {
+        this.nome = nome;
+	this.sexo = sexo;
+	this.cpf = cpf;
+	this.telefone = telefone;
+	this.endereco = endereco;
+    }
+
+
+    public String getNome() {
+	return nome;
+    }
+
+
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
+
+
+    public String getSexo() {
+	return sexo;
+    }
+
+
+    public void setSexo(String sexo) {
+	this.sexo = sexo;
+    }
+
+
+    public String getCpf() {
+	return cpf;
+    }
+
+
+    public void setCpf(String cpf) {
+    	this.cpf = cpf;
+    }
+
+
+    public String getTelefone() {
+	return telefone;
+    }
+
+
+    public void setTelefone(String telefone) {
+	this.telefone = telefone;
+    }
+
+
+    public String getEndereco() {
+	return endereco;
+	}
+
+
+    public void setEndereco(String endereco) {
+	this.endereco = endereco;
+	}
+
+
+    public String toString() {
+	return "Gestor: \nNome= " + nome + "\nSexo= " + sexo + "\nCPF= " + cpf + "\nTelefone= " + telefone + "\nEndereco= "
+	+ endereco;
+    }
+    
+}
+
