0164c6bc9733a317b4dde58b303c3a1bca5f613f
Lucas Lins <lucas11lins@gmail.com>
2021-05-19 09:48:23 -0300

    Classes atualizadas, interface atualizada

--- src/model/Doacao.java
+++ src/model/Doacao.java
@@ -1,79 +1,90 @@
 
 package model;
 
+import java.util.ArrayList;
+
 public class Doacao {
     private Voluntario vol;
     private double doacaoDinheiro = 0;
-    private String doacaoProduto;
+    private ArrayList<Item> itens;
     private boolean repetir;
     private String modoEntrega;
     private boolean entregue;
     private String data;
-
-    public Doacao(Voluntario vol, String doacaoProduto, boolean repetir, String modoEntrega, String data) {
-        this.vol = vol;
-        this.doacaoProduto = doacaoProduto;
-        this.repetir = repetir;
-        this.modoEntrega = modoEntrega;
-        this.data = data;
-    }
     
-    public Doacao(){}
+	public Doacao(Voluntario vol, double doacaoDinheiro, ArrayList<Item> itens, boolean repetir, String modoEntrega,
+			boolean entregue, String data) {
+		super();
+		this.vol = vol;
+		this.doacaoDinheiro = doacaoDinheiro;
+		this.itens = itens;
+		this.repetir = repetir;
+		this.modoEntrega = modoEntrega;
+		this.entregue = entregue;
+		this.data = data;
+	}
 
-    public Voluntario getVol() {
-        return vol;
-    }
+    public Doacao() {}
 
-    public void setVol(Voluntario vol) {
-        this.vol = vol;
-    }
+	public Voluntario getVol() {
+		return vol;
+	}
 
-    public double getDoacaoDinheiro() {
-        return doacaoDinheiro;
-    }
+	public void setVol(Voluntario vol) {
+		this.vol = vol;
+	}
 
-    public void setDoacaoDinheiro(double doacaoDinheiro) {
-        this.doacaoDinheiro = doacaoDinheiro;
-    }
+	public double getDoacaoDinheiro() {
+		return doacaoDinheiro;
+	}
 
-    public String getDoacaoProduto() {
-        return doacaoProduto;
-    }
+	public void setDoacaoDinheiro(double doacaoDinheiro) {
+		this.doacaoDinheiro = doacaoDinheiro;
+	}
 
-    public void setDoacaoProduto(String doacaoProduto) {
-        this.doacaoProduto = doacaoProduto;
-    }
+	public ArrayList<Item> getItens() {
+		return itens;
+	}
 
-    public boolean getRepetir() {
-        return repetir;
-    }
+	public void setItens(ArrayList<Item> itens) {
+		this.itens = itens;
+	}
 
-    public void setRepetir(boolean repetir) {
-        this.repetir = repetir;
-    }
+	public boolean isRepetir() {
+		return repetir;
+	}
 
-    public String getModoEntrega() {
-        return modoEntrega;
-    }
+	public void setRepetir(boolean repetir) {
+		this.repetir = repetir;
+	}
 
-    public void setModoEntrega(String modoEntrega) {
-        this.modoEntrega = modoEntrega;
-    }
+	public String getModoEntrega() {
+		return modoEntrega;
+	}
 
-    public String getData() {
-        return data;
-    }
+	public void setModoEntrega(String modoEntrega) {
+		this.modoEntrega = modoEntrega;
+	}
 
-    public void setData(String data) {
-        this.data = data;
-    }
+	public boolean isEntregue() {
+		return entregue;
+	}
 
-    public boolean getEntregue() {
-        return entregue;
-    }
+	public void setEntregue(boolean entregue) {
+		this.entregue = entregue;
+	}
 
-    public void setEntregue(boolean entregue) {
-        this.entregue = entregue;
-    }
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Doacao{" + "vol=" + vol + ", doacaoDinheiro=" + doacaoDinheiro + ", itens=" + itens + ", repetir=" + repetir + ", modoEntrega=" + modoEntrega + ", entregue=" + entregue + ", data=" + data + '}';
+	}
 
 }
